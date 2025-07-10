bffdd3bdaf11b6280d603d1882f9d8e5c5d294a4
Dipesh Kumar Sah <dipesh@rumsan.net>
2025-07-10 20:36:57 +0545

    fix: update redis config

--- server/src/config/redis.config.ts
+++ server/src/config/redis.config.ts
@@ -4,6 +4,8 @@ const redisConfig: ConnectionOptions = {
   host: process.env.REDIS_HOST || 'localhost',
   port: parseInt(process.env.REDIS_PORT || '6379'),
   password: process.env.REDIS_PASSWORD,
+  username: process.env.REDIS_USERNAME,
+  tls: {},
   retryDelayOnFailover: 100,
   enableReadyCheck: false,
   lazyConnect: true,
